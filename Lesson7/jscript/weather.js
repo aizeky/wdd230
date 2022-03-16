2afac53fd6aa2cc10a4ff6d377dae8fd496282a9
aizeky <man17008@byui.edu>
2022-03-16 00:37:52 -0600

    Yasss

--- Lesson7/jscript/weather.js
+++ Lesson7/jscript/weather.js
@@ -5,7 +5,9 @@ fetch(url)
   .then((jsObject) => {
     console.log(jsObject);
     document.querySelector('#current-temp').textContent = jsObject.main.temp;
-    document.querySelector('#windChill').textContent = jsObject.main.feels_like;
+    document.querySelector('#speed').textContent = 'Wind Speed: ' + jsObject.wind.speed;
+    var wchill = (35.74 + (0.6215 * jsObject.main.temp))-(35.75 * Math.pow(jsObject.wind.speed,0.16)) + (0.4275*jsObject.main.temp*Math.pow(jsObject.wind.speed,0.16));
+    document.querySelector('#windChill').textContent = wchill.toFixed(2);
 
     const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description;
