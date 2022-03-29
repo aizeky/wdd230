084f92952a839dc55c6211b910d8e32a26013ab8
aizeky <man17008@byui.edu>
2022-03-28 22:19:05 -0600

    Yasss Queen

---
+++ Lesson7/jscript/join.js
@@ -0,0 +1,27 @@
+const weekDay = new Date();
+
+
+let currentdate= document.lastModified;
+document.getElementById('modified').textContent = 'Last Updated: '+ currentdate + ".";
+
+const hamburgerBtn = document.querySelector('.ham');
+const navigation = document.querySelector('.navigation');
+
+hamburgerBtn.addEventListener('click', ()=> {navigation.classList.toggle('responsive')}, false);
+
+const datefield = document.querySelector("date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+dateStyle: "full"
+}).format(now);
+
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
